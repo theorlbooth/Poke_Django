ce14007def4be60bd493baaca7c7552b40e570ea
Theo Booth <theorlbooth@googlemail.com>
2021-03-23 11:00:31 +0000

    Authentication

--- jwt_auth/admin.py
+++ jwt_auth/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import User
+from django.contrib.auth import get_user_model
 
-# admin.site.register(User)
+User = get_user_model()
+
+admin.site.register(User)
